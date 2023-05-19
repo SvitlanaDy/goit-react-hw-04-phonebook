cc321765b0f706a87e020151ed36f161cb14d9b9
Svitlana Dyshkant <110805508+SvitlanaDy@users.noreply.github.com>
2023-05-19 18:14:30 +0300

    upd App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,20 +6,16 @@ import Filter from 'components/filter/Filter';
 import css from './App.module.css';
 
 const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(
+    () =>
+      JSON.parse(localStorage.getItem('contacts')) || [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+  );
   const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    const savedContacts = localStorage.getItem('contacts');
-    return(
-      setContacts(JSON.parse(savedContacts)));
-    
-  }, []);
 
   useEffect(() => {
     contacts.length &&
